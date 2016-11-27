b265ec6e646ae3af8e3ac66cc26d51eb4cc55ddd
Karol Brzeski <karol.brzeski91@gmail.com>
2016-11-27 20:35:47 +0100

    Amendments stylistic

--- public_html/js/animate.js
+++ public_html/js/animate.js
@@ -2,5 +2,3 @@ $('.main-slider-content h1').addClass('animated fadeInDown');
 $('.main-slider-content p').addClass('animated fadeInLeft');
 $('.main-slider-content img').addClass('animated zoomIn');
 $('.modal-content').addClass('animated slideInDown');
-$('.photo-banner-inner h2').addClass('animated bounceInLeft');
-$('.photo-banner-inner p').addClass('animated bounceInLeft');
